2cd758773475e5fd926723243abf5ca685e6cc3c
NISARGA <nisarga7419@gmail.com>
2026-02-26 13:06:37 +0530

    added JENKINS

---
+++ src/Nisarga/JENKINS.java
@@ -0,0 +1,11 @@
+package Nisarga;
+
+public class JENKINS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First jenkins code");
+		System.out.println("First line");
+	}
+
+}
